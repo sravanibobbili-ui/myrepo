404f9c0f2242843389fe578ea56df7677d615f19
sravanibobbili-ui <101252870+sravanibobbili-ui@users.noreply.github.com>
2022-03-11 11:04:33 +0530

    Update App.js

--- ReactJS/frontend/src/App.js
+++ ReactJS/frontend/src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Erconn from "./Erconn";
 import * as ReactDOM from "react-dom";
-// import Split from "react-split";
 import SplitPane from "react-split-pane";
-// import display from "./Erconn";
-// import "./navbar.jsx";
 
 class App extends Component {
   state = {
@@ -16,17 +13,14 @@ class App extends Component {
   async componentDidMount() {
     const response = await fetch("http://localhost:7890/api/getnode");
     const val = await response.json();
-    // console.log(val);
     if (response.status !== 200) {
       throw Error(val.message);
     }
     this.setState({ data: val });
-    // console.log(this.state.data);
   }
 
   async display(val) {
     const response = await fetch("http://localhost:7891/api/getdata");
-    // console.log(response);
     const value = await response.json();
     console.log(val);
     this.setState({ res: value });
@@ -50,34 +44,18 @@ class App extends Component {
         nodesdata["eas"] = eas;
         nodesdata["nru"] = nru;
         return nodesdata;
-        // ReactDOM.render(<Erconn data={blades} />);
       }
-      // console.log(n);
     }
   }
   clickHandler = async (e) => {
-    // this.setState({ res: e.target.id });
-
-    // console.log(e.target.id);
     e.preventDefault();
 
     console.log("clicked");
     let a = await this.display(e.target.id);
-    // console.log(a);
-    let msg = e.target.id;
     const el = document.getElementById("bottom");
     ReactDOM.render(<Erconn data={a} />, el);
-    // <Erconn msg={e.target.id} />;
-    // console.log(msg);
   };
-
-  // console.log(e.target.id);
-  // this.setState({ res: e.target.id });
-  // console.log(this.props.res);
-  // console.log(this.state.res);
-  //api call  --- set data to pps
-
-  render() {
+ render() {
     return (
       <div className="header">
         <header className="App-header">
@@ -91,14 +69,7 @@ class App extends Component {
             defaultSize={200}
             maxSize={400}
             type="Resizer"
-            attribute="vertical"
-
-            // className="split"
-            // cursor="col-resize"
-            // snapOffset={2}
-            // gutterSize={5}
-            // style={{ height: `calc(100vh - 4rem)` }}
-          >
+            attribute="vertical">
             <div className="Node_link">
               {this.state.data.map((value) => (
                 <>
@@ -106,7 +77,7 @@ class App extends Component {
                     onClick={this.clickHandler}
                     id={value}
                     href={value}
-                    // onClick={this.clickHandler}
+                  
                     className="section-values"
                   >
                     {value}
